b31ac21bde14a7867c2ac084ede824ed19071c21
Med Dhia Bouthouri <med.d.bouthuri@gmail.com>
2025-09-29 22:06:10 +0100

    main(feat):  integrated/wrapped app inside react query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import DataExplorer from './pages/DataExplorer.jsx'
 
@@ -8,8 +9,12 @@ const router = createBrowserRouter([
   { path: '/', element: <DataExplorer /> },
 ])
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 )
